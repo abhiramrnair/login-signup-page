44a97f2c6905fe4edc698308b2db5cff00ef293c
Abhiram R Nair <98166342+abhiramrnair@users.noreply.github.com>
2025-04-04 18:22:25 +0530

    Update login.js

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -25,7 +25,7 @@ function Login() {
       return handleError('All details are required!')
     }
     try{
-      const url="backend-rouge-seven-44.vercel.app/auth/login";
+      const url="https://backend-rouge-seven-44.vercel.app/auth/login";
       const response=await fetch(url, {
         method: "POST", 
         headers: {
